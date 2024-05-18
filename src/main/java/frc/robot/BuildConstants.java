3a5cabf740af077fb8989e699d09de28e2b928e5
Adhersh1 <adhersh@ucsc.edu>
2024-05-18 09:18:38 -0700

    adding field constants and drive constants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024_Lepidopterist";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 30;
-  public static final String GIT_SHA = "b572744f2461ad991e1fbeb7ea91e2c2f1b096a9";
-  public static final String GIT_DATE = "2024-05-16 18:32:56 EDT";
+  public static final int GIT_REVISION = 32;
+  public static final String GIT_SHA = "d6dc692f3caeeaa80dcf7baaee9de774570fa3bf";
+  public static final String GIT_DATE = "2024-05-17 01:52:17 EDT";
   public static final String GIT_BRANCH = "OldOldX3MergededBranch";
-  public static final String BUILD_DATE = "2024-05-16 18:52:59 EDT";
-  public static final long BUILD_UNIX_TIME = 1715899979013L;
+  public static final String BUILD_DATE = "2024-05-18 01:06:45 EDT";
+  public static final long BUILD_UNIX_TIME = 1716008805612L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
---
+++ src/main/java/frc/robot/FieldConstants.java
@@ -0,0 +1,189 @@
+// Copyright (c) 2024 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package frc.robot;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.math.geometry.*;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Filesystem;
+import java.io.IOException;
+import java.nio.file.Path;
+import lombok.Getter;
+
+/**
+ * Contains various field dimensions and useful reference points. Dimensions are in meters, and sets
+ * of corners start in the lower left moving clockwise. <b>All units in Meters</b> <br>
+ * <br>
+ *
+ * <p>All translations and poses are stored with the origin at the rightmost point on the BLUE
+ * ALLIANCE wall.<br>
+ * <br>
+ * Length refers to the <i>x</i> direction (as described by wpilib) <br>
+ * Width refers to the <i>y</i> direction (as described by wpilib)
+ */
+public class FieldConstants {
+  public static final double fieldLength = Units.inchesToMeters(651.223);
+  public static final double fieldWidth = Units.inchesToMeters(323.277);
+  public static final double wingX = Units.inchesToMeters(229.201);
+  public static final double podiumX = Units.inchesToMeters(126.75);
+  public static final double startingLineX = Units.inchesToMeters(74.111);
+
+  public static final Translation2d ampCenter =
+      new Translation2d(Units.inchesToMeters(72.455), fieldWidth);
+
+  /** Staging locations for each note */
+  public static final class StagingLocations {
+    public static final double centerlineX = fieldLength / 2.0;
+
+    // need to update
+    public static final double centerlineFirstY = Units.inchesToMeters(29.638);
+    public static final double centerlineSeparationY = Units.inchesToMeters(66);
+    public static final double spikeX = Units.inchesToMeters(114);
+    // need
+    public static final double spikeFirstY = Units.inchesToMeters(161.638);
+    public static final double spikeSeparationY = Units.inchesToMeters(57);
+
+    public static final Translation2d[] centerlineTranslations = new Translation2d[5];
+    public static final Translation2d[] spikeTranslations = new Translation2d[3];
+
+    static {
+      for (int i = 0; i < centerlineTranslations.length; i++) {
+        centerlineTranslations[i] =
+            new Translation2d(centerlineX, centerlineFirstY + (i * centerlineSeparationY));
+      }
+    }
+
+    static {
+      for (int i = 0; i < spikeTranslations.length; i++) {
+        spikeTranslations[i] = new Translation2d(spikeX, spikeFirstY + (i * spikeSeparationY));
+      }
+    }
+  }
+
+  /** Each corner of the speaker * */
+  public static final class Speaker {
+
+    // corners (blue alliance origin)
+    public static final Translation3d topRightSpeaker =
+        new Translation3d(
+            Units.inchesToMeters(18.055),
+            Units.inchesToMeters(238.815),
+            Units.inchesToMeters(83.091));
+
+    public static final Translation3d topLeftSpeaker =
+        new Translation3d(
+            Units.inchesToMeters(18.055),
+            Units.inchesToMeters(197.765),
+            Units.inchesToMeters(83.091));
+
+    public static final Translation3d bottomRightSpeaker =
+        new Translation3d(0.0, Units.inchesToMeters(238.815), Units.inchesToMeters(78.324));
+    public static final Translation3d bottomLeftSpeaker =
+        new Translation3d(0.0, Units.inchesToMeters(197.765), Units.inchesToMeters(78.324));
+
+    /** Center of the speaker opening (blue alliance) */
+    public static final Translation3d centerSpeakerOpening =
+        bottomLeftSpeaker.interpolate(topRightSpeaker, 0.5);
+  }
+
+  public static final class Subwoofer {
+    public static final Pose2d ampFaceCorner =
+        new Pose2d(
+            Units.inchesToMeters(35.775),
+            Units.inchesToMeters(239.366),
+            Rotation2d.fromDegrees(-120));
+
+    public static final Pose2d sourceFaceCorner =
+        new Pose2d(
+            Units.inchesToMeters(35.775),
+            Units.inchesToMeters(197.466),
+            Rotation2d.fromDegrees(120));
+
+    public static final Pose2d centerFace =
+        new Pose2d(
+            Units.inchesToMeters(35.775),
+            Units.inchesToMeters(218.416),
+            Rotation2d.fromDegrees(180));
+  }
+
+  public static final class Stage {
+    public static final Pose2d podiumLeg =
+        new Pose2d(Units.inchesToMeters(126.75), Units.inchesToMeters(161.638), new Rotation2d());
+    public static final Pose2d ampLeg =
+        new Pose2d(
+            Units.inchesToMeters(220.873),
+            Units.inchesToMeters(212.425),
+            Rotation2d.fromDegrees(-30));
+    public static final Pose2d sourceLeg =
+        new Pose2d(
+            Units.inchesToMeters(220.873),
+            Units.inchesToMeters(110.837),
+            Rotation2d.fromDegrees(30));
+
+    public static final Pose2d centerPodiumAmpChain =
+        new Pose2d(
+            podiumLeg.getTranslation().interpolate(ampLeg.getTranslation(), 0.5),
+            Rotation2d.fromDegrees(120.0));
+    public static final Pose2d centerAmpSourceChain =
+        new Pose2d(
+            ampLeg.getTranslation().interpolate(sourceLeg.getTranslation(), 0.5), new Rotation2d());
+    public static final Pose2d centerSourcePodiumChain =
+        new Pose2d(
+            sourceLeg.getTranslation().interpolate(podiumLeg.getTranslation(), 0.5),
+            Rotation2d.fromDegrees(240.0));
+    public static final Pose2d center =
+        new Pose2d(Units.inchesToMeters(192.55), Units.inchesToMeters(161.638), new Rotation2d());
+    public static final double centerToChainDistance =
+        center.getTranslation().getDistance(centerPodiumAmpChain.getTranslation());
+  }
+
+  public static final class Amp {
+    public static final Translation2d ampTapeTopCorner =
+        new Translation2d(Units.inchesToMeters(130.0), Units.inchesToMeters(305.256));
+  }
+
+  public static final double aprilTagWidth = Units.inchesToMeters(6.50);
+  public static final AprilTagLayoutType defaultAprilTagType = AprilTagLayoutType.OFFICIAL;
+
+  @Getter
+  public enum AprilTagLayoutType {
+    OFFICIAL("2024-official"),
+    SPEAKERS_ONLY("2024-speakers"),
+    AMPS_ONLY("2024-amps"),
+    WPI("2024-wpi");
+
+    private AprilTagLayoutType(String name) {
+      if (Constants.disableHAL) {
+        layout = null;
+      } else {
+        try {
+          layout =
+              new AprilTagFieldLayout(
+                  Path.of(Filesystem.getDeployDirectory().getPath(), "apriltags", name + ".json"));
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      if (layout == null) {
+        layoutString = "";
+      } else {
+        try {
+          layoutString = new ObjectMapper().writeValueAsString(layout);
+        } catch (JsonProcessingException e) {
+          throw new RuntimeException(
+              "Failed to serialize AprilTag layout JSON " + toString() + "for Northstar");
+        }
+      }
+    }
+
+    private final AprilTagFieldLayout layout;
+    private final String layoutString;
+  }
+}
