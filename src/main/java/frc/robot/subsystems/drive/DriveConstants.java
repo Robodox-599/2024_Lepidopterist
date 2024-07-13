320c914fe64bf512cc765e5a6634dd2d0f6ae9fb
Adhersh1 <adhersh@ucsc.edu>
2024-07-13 12:35:21 -0700

    COMMETNS GALORE

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -10,14 +10,27 @@ import edu.wpi.first.math.util.Units;
 
 public final class DriveConstants {
 
+  /*
+   * THIS IS THE DEVICE ID AND NETWORK NAME THAT IS ATTACHED TO THE PIGEON 2.0 AND CANIVORE
+   * TO FIND THE ID AND NETWORK NAME GO TO PHOENIX TUNER AND FLASH THE DEVICES TO ENSURE THAT THE CORRECT DEVICE IS SELECTED
+   *
+   * ! DO NOT CHANGE DURING COMP !
+   */
+
   // Canbus Network Name
-  // You must specify the canbus name that the devices are on
 
   public static final String canbus = "LunaDriveCANivore";
 
   // Gyro Device ID
 
   public static final int gyro = 12;
+
+  /*
+   * THESE ARE THE DEVICE IDS FOR THE MOTORS AND ENCODERS THAT ARE ATTACHED TO THE SWERVE MODULES
+   * TO FIND THE IDS GO TO PHOENIX TUNER AND FLASH THE DEVICES TO ENSURE THAT THE CORRECT DEVICE IS SELECTED
+   *
+   * ! DO NOT CHANGE DURING COMP !
+   */
 
   // Module 0 (FL) Device IDs
 
@@ -55,13 +68,29 @@ public final class DriveConstants {
   public static final int DriveMotorSupplyCurrentLimitConstant = 40;
   public static final int TurnMotorSupplyCurrentLimitConstant = 30;
 
+  /*
+   * EACH SWERVE MODULE HAS A GEAR RATIO (L1, L2, L3, OR L4)
+   *
+   * !! DO NOT CHANGE THESE VALUES WITHOUT FIRST CONSULTING MEER !!
+   */
+
   // Drive and Turn Motor Gear Ratio L3
 
+  // DRIVE GEAR RATIO
   public static final double DriveGearRatioConstant =
       ((50.0 / 14.0) * (17.0 / 27.0) * (45.0 / 15.0));
+
+  // TURNING GEAR RATIO
   public static final double TurnGearRatioConstant = (150.0 / 7.0);
 
-  // Constants for Maximum linear speed, track width, and drive base radius
+  /*
+   * THESE ARE THE CONSTANTS THAT TELL THE ROBOT ITS MAXIMUM LINEAR SPEED, TRACK WIDTH, AND DRIVE BASE RADIUS
+   * TUNE THESE VALUES ACCORDINGLY.
+   *
+   * !! DO NOT TUNE DURING COMP OR IN COMP BRANCH !!
+   *
+   * IF THERE IS A NEED TO CHANGE THESE VALUES DURING COMP THEN PLEASE ASK MEER
+   */
 
   public static final double MAX_LINEAR_SPEED = Units.feetToMeters(15.725065833333);
   public static final double TRACK_WIDTH_X = Units.inchesToMeters(20.749988795006054);
@@ -73,60 +102,134 @@ public final class DriveConstants {
   public static final double MAX_ANGULAR_ACCELERATION = MAX_LINEAR_ACCELERATION / DRIVE_BASE_RADIUS;
   public static final double MAX_AUTOAIM_SPEED = MAX_LINEAR_SPEED / 4;
 
-  // Drive Path Following Constants
+  /*
+   * IMPORTANT: commented out because untested on real.
+   * TODO: add logic to make it pick the correct PID for sim or real.
+   *
+   * PID CONSTANTS FOR PATH FOLLOWING
+   * TUNE THESE VALUES ACCORDINGLY
+   *
+   * WILL OVERRIDE DRIVE, SIM, AND REPLAY PID VALUES
+   *
+   * !! DO NOT TUNE DURING COMP OR IN COMP BRANCH !!
+   */
 
-  // IMPORTANT: commented out because untested on real.
-
-  // TODO: add logic to make it pick the correct PID for sim or real.
-
+  // PATH FOLLOW TRANSLATION KP
   // public static final double pathFollowTranslationkP = 15.0;
+
+  // PATH FOLLOW TRANSLATION KI
   // public static final double pathFollowTranslationkI = 0.15;
+
+  // PATH FOLLOW TRANSLATION KD
   // public static final double pathFollowTranslationkD = 0.0;
 
+  // PATH FOLLOW ROTATION KP
   // public static final double pathFollowRotationkP = 20.0;
+
+  // PATH FOLLOW ROTATION KI
   // public static final double pathFollowRotationkI = 0.1;
+
+  // PATH FOLLOW ROTATION KD
   // public static final double pathFollowRotationkD = 0.0;
 
-  // Drive Replay/Real/Teleop constants
+  /*
+   * PID FOR LIVE/REAL ROBOT AND REPLAY MODE ROBOT
+   * TUNE THESE VALUES ACCORDINGLY
+   *
+   * !! DO NOT TUNE DURING COMP OR IN COMP BRANCH !!
+   */
 
+  // REAL: KS SIMPLE FEEDFORWARD
   public static final double driveRealFeedFowardkS = 0.080108;
+
+  // REAL: KP SIMPLE FEEDFORWARD
   public static final double driveRealFeedFowardkV = 0.61952;
+
+  // REAL: KA SIMPLE FEEDFORWARD
   public static final double driveRealFeedFowardkA = 0.019975;
 
+  // REAL: KP DRIVE MOTOR
   public static final double driveRealFeedBackkP = 5.0;
+
+  // REAL: KI DRIVE MOTOR
   public static final double driveRealFeedBackkI = 0.0;
+
+  // REAL: KD DRIVE MOTOR
   public static final double driveRealFeedBackkD = 0.0;
 
+  // REAL: KP TURN MOTOR
   public static final double turnRealFeedbackkP = 0.1;
+
+  // REAL: KI TURN MOTOR
   public static final double turnRealFeedbackkI = 0.0;
+
+  // REAL: KD TURN MOTOR
   public static final double turnRealFeedbackkD = 0.0;
 
-  // Drive Sim constants
+  /*
+   * PID FOR SIMULATED ROBOT
+   * TUNE THESE VALUES ACCORDINGLY
+   *
+   * !! DO NOT TUNE DURING COMP OR IN COMP BRANCH !!
+   */
 
+  // SIM: KS SIMPLE FEEDFORWARD
   public static final double driveSimFeedFowardkS = 0.0;
+
+  // SIM: KV SIMPLE FEEDFORWARD
   public static final double driveSimFeedFowardkV = 0.13;
+
+  // SIM: KA SIMPLE FEEDFORWARD
   public static final double driveSimFeedFowardkA = 0.0;
 
+  // SIM: KP DRIVE MOTOR
   public static final double driveSimFeedBackkP = 0.1;
+
+  // SIM: KI DRIVE MOTOR
   public static final double driveSimFeedBackkI = 0.0;
+
+  // SIM: KD DRIVE MOTOR
   public static final double driveSimFeedBackkD = 0.0;
 
+  // SIM: KP TURN MOTOR
   public static final double turnSimFeedbackkP = 10;
+
+  // SIM: KI TURN MOTOR
   public static final double turnSimFeedbackkI = 0.0;
+
+  // SIM: KD TURN MOTOR
   public static final double turnSimFeedbackkD = 0.0;
+
+  /*
+   * CONSTANTS FOR CAMERAS
+   *
+   * !! DO NOT TUNE DURING COMP OR IN COMP BRANCH !!
+   */
 
   // Camera 1 (FL) Constants
 
+  // CAMERA 1 NAME
   public static final String camera1Name = "Camera One";
 
+  // CAMERA 1 POSE (X)
   public static final double camera1PoseX = Units.inchesToMeters(8.875);
+
+  // CAMERA 1 POSE (Y)
   public static final double camera1PoseY = Units.inchesToMeters(10.5);
+
+  // CAMERA 1 POSE (Z)
   public static final double camera1PoseZ = Units.inchesToMeters(8.25);
 
+  // CAMERA 1 POSE (ROLL)
   public static final double camera1PoseRoll = Units.degreesToRadians(0);
+
+  // CAMERA 1 POSE (PITCH)
   public static final double camera1PosePitch = Units.inchesToMeters(-28.125);
+
+  // CAMERA 1 POSE (YAW)
   public static final double camera1PoseYaw = Units.inchesToMeters(30);
 
+  // CAMERA 1 DISTORTION MATRIX
   public static final Matrix<N3, N3> CAMERA_ONE_MATRIX =
       MatBuilder.fill(
           Nat.N3(),
@@ -141,6 +244,7 @@ public final class DriveConstants {
           0.0,
           1.0);
 
+  // CAMERA 1 DISTANCE COEFFICIENTS
   public static final Matrix<N5, N1> CAMERA_ONE_DIST_COEFFS =
       MatBuilder.fill(
           Nat.N5(),
@@ -151,17 +255,28 @@ public final class DriveConstants {
           6.491281122640802E-4,
           0.03731824873787814); // Last 3 values have been truncated
 
-  // Camera 2 (FR) Constants
+  // CAMERA 2 NAME
   public static final String camera2Name = "Camera Two";
 
+  // CAMERA 2 POSE (X)
   public static final double camera2PoseX = Units.inchesToMeters(8.875);
+
+  // CAMERA 2 POSE (Y)
   public static final double camera2PoseY = Units.inchesToMeters(-10.5);
+
+  // CAMERA 2 POSE (Z)
   public static final double camera2PoseZ = Units.inchesToMeters(8.25);
 
+  // CAMERA 2 POSE (ROLL)
   public static final double camera2PoseRoll = Units.degreesToRadians(0);
+
+  // CAMERA 2 POSE (PITCH)
   public static final double camera2PosePitch = Units.inchesToMeters(-28.125);
+
+  // CAMERA 2 POSE (YAW)
   public static final double camera2PoseYaw = Units.inchesToMeters(-30);
 
+  // CAMERA 2 DISTORTION MATRIX
   public static final Matrix<N3, N3> CAMERA_TWO_MATRIX =
       MatBuilder.fill(
           Nat.N3(),
@@ -176,6 +291,7 @@ public final class DriveConstants {
           0.0,
           1.0);
 
+  // CAMERA 2 DISTANCE COEFFICIENTS
   public static final Matrix<N5, N1> CAMERA_TWO_DIST_COEFFS =
       MatBuilder.fill(
           Nat.N5(),
@@ -185,6 +301,14 @@ public final class DriveConstants {
           -6.669129885790735E-4,
           6.491281122640802E-4,
           0.03731824873787814); // Last 3 values have been truncated
-  // odom constants
+
+  /*
+   * ODOMETRY FREQUENCY
+   *
+   * TODO: CHANGE FREQUENCY BASED ON SIM OR REAL. SET SIM TO 50 AND SET REAL TO 250.
+   *
+   * !! DO NOT TUNE DURING COMP OR IN COMP BRANCH !!
+   */
+
   public static final int odomFrequency = 250;
 }
