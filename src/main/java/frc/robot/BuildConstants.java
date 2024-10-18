7f517f53f3cd755c1972989ca2a1c664543f1b72
Adhersh1 <adhersh@ucsc.edu>
2024-10-17 23:41:16 -0700

    sim actually starts now :)

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024_Lepidopterist";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 151;
-  public static final String GIT_SHA = "0eb0a460ce9b61577a8ebdf3ad7284190f65d1e2";
-  public static final String GIT_DATE = "2024-10-18 02:33:16 EDT";
+  public static final int GIT_REVISION = 152;
+  public static final String GIT_SHA = "2b6058cd9a5e44f393e0bab6212943dd70cd13b6";
+  public static final String GIT_DATE = "2024-10-18 02:39:19 EDT";
   public static final String GIT_BRANCH = "TidalPractice";
-  public static final String BUILD_DATE = "2024-10-18 02:39:04 EDT";
-  public static final long BUILD_UNIX_TIME = 1729233544521L;
+  public static final String BUILD_DATE = "2024-10-18 02:40:43 EDT";
+  public static final long BUILD_UNIX_TIME = 1729233643369L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
